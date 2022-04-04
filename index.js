c837af9e69b0f103d2c9e8e1541c141015f7fc8e
Illia <ilya.kvitkovskyi@gmail.com>
2022-04-04 13:31:02 +0300

    added modal window with local storage

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-$(document).ready(function () {
-  $('#setCookie').click(function () {
-    var date = new Date();
-    date.setTime(date.getTime() + 60 * 1000);
-    $.cookie('popup', '', { expires: date, path: '/' });
-    $('#bg_popup').hide();
-  });
+document.addEventListener('DOMContentLoaded', function () {
+  const createItemInLocalStorage = (currentDate) => {
+    
+    document.querySelector('.bg_popup').style.display = 'block';
+    
 
-  if ($.cookie('popup') == null) {
-    setTimeout(function () {
-      $('#bg_popup').show();
-    }, 2000);
+    $('.setCookie').click(function () {
+      let date = new Date(currentDate);
+      date.setTime(date.getTime() + 3 * 60 * 60 * 1000);
+
+      localStorage.setItem('expiresAt', date);
+      
+      document.querySelectorAll('bg_popup').forEach((box) => {
+        box.style.display = 'none';
+      });
+    });
+  };
+
+  const expiresAtValue = localStorage.getItem('expiresAt');
+  let currentDate = new Date();
+
+  if (!expiresAtValue) {
+    createItemInLocalStorage(currentDate);
   } else {
-    $('#bg_popup').hide();
+    const expiresDate = new Date(expiresAtValue);
+    if (expiresDate - currentDate < 0) {
+      createItemInLocalStorage(currentDate);
+    }
   }
 });
+
+
+
